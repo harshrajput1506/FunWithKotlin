f2b6feeebb381aaa42e9a9e5a515890b99673c38
harshrajput1506 <harshrajput2906@gmail.com>
2023-08-28 13:53:19 +0530

    loops

--- src/main/kotlin/Loops.kt
+++ src/main/kotlin/Loops.kt
@@ -1,5 +1,9 @@
 fun main() {
     for (i in 1..5 ) println(i)
     println("Until")
-    for (i in 1 until 10) println(i)
+    for (i in -2 until 10) println(i)
+    println("Steps")
+    for (i in 1..10 step 2) println(i)
+    println("Down to")
+    for (i in 10 downTo -3) println(i)
 }
